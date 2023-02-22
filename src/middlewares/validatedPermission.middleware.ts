78baa1480c3772e6c2493f326bad1dd530bdc2ba
Andrew <andrewairamdasilva@gmail.com>
2023-02-22 14:49:41 -0300

    fix: delete correct

---
+++ src/middlewares/validatedPermission.middleware.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from "express";
+import { QueryResult } from "pg";
+import format from "pg-format";
+import { client } from "../database";
+import { AppError } from "../errors";
+
+const verify = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response | void> => {
+  const userId: number = Number(req.params.id);
+  const admin: boolean = req.user.admin;
+  const { id } = req.user;
+  console.log(admin, userId);
+
+  if (admin === false) {
+    const queryUserTemplate: string = `
+          SELECT
+            *
+          FROM
+            users
+          WHERE
+            id = (%L)
+        `;
+    const queryUserFormat: string = format(queryUserTemplate, [id]);
+    const queryUserResult: QueryResult = await client.query(queryUserFormat);
+    const userVerify = queryUserResult.rows[0];
+
+    if (userId !== id) {
+      throw new AppError("Insufficient Permission", 403);
+    }
+  }
+
+  return next();
+};
+
+export default { verify };
