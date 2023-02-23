54d7e785748859bc3b52a9757f56331fca6e5542
Andrew <andrewairamdasilva@gmail.com>
2023-02-23 13:12:40 -0300

    fix:removed regexUserSchema

--- src/schemas/users.schemas.ts
+++ src/schemas/users.schemas.ts
@@ -6,13 +6,6 @@ const createUserSchema = z.object({
   email: z.string().email(),
   password: z
     .string()
-    .regex(new RegExp(".*[A-Z].*"), "One uppercase character")
-    .regex(new RegExp(".*[a-z].*"), "One lowercase character")
-    .regex(new RegExp(".*\\d.*"), "One number")
-    .regex(
-      new RegExp(".*[`~<>?,./!@#$%^&*()\\-_+=\"'|{}\\[\\];:\\\\].*"),
-      "One special character"
-    )
     .min(8, "Must be at least 8 characters in length")
     .transform((pass) => {
       return hashSync(pass, 10);
